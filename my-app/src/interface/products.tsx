97371c99dfab0e5e11c325eecd119379b46aa76b
Gubber41 <minghanlow@gmail.com>
2024-02-06 03:11:06 +1100

    Redesign of product page
    Converted from in-built html table layout to just divs with appropriate formatting.

--- my-app/src/interface/products.tsx
+++ my-app/src/interface/products.tsx
@@ -1,7 +1,9 @@
-import React, { FormEvent, useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState, useRef } from "react";
 import Sidebar from "../sidebar/sidebar";
 import api from "../api";
-import { JsxElement } from "typescript";
+import { JsxElement, canHaveModifiers } from "typescript";
+const MAXNAMELEN = 245;
+const MAXDESCLEN = 331;
 
 interface product {
     p_id: number;
@@ -12,6 +14,26 @@ interface product {
     is_available: boolean;
 }
 
+// Gets font from body
+function getTextWidth(text: string): number {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    
+    if (!context) {
+        return 0;
+    }
+    context.font = getComputedStyle(document.body).font;
+  
+    return context.measureText(text).width;
+}
+
+// From stackoverflow
+// (https://stackoverflow.com/questions/2901102/how-to-format-a-number-with-commas-as-thousands-separators)
+// 6/02/2024
+function numberDelimiter(x: number) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 
 function Products() {
     const displayedColumns = [
@@ -20,7 +42,17 @@ function Products() {
         "price",
         "source",
         "is_available",
+        "options",
     ];
+
+    const widths: any = {
+        "name": "w-3/12",
+        "description": "w-4/12",
+        "price": "w-1/12",
+        "source": "w-2/12",
+        "is_available": "w-1/12",
+        "options": "w-1/12",
+    }
 
     const [products, setProducts] = useState([]);
 
@@ -44,9 +76,9 @@ function Products() {
         }
         
         let styles: any = {
-            name: " w-2/12",
+            name: " w-1/12",
         }
-
+        
         return (
         <tr className="">
             {displayedColumns.map((colName: string) => {
@@ -55,39 +87,103 @@ function Products() {
         </tr>
         );
     }
+
+    
+    // Formats the data 
+    function formatData(rawData: any, colName: string): string {
+        
+        let i: number = 0;
+        switch (colName) {
+            case "name":
+                var formattedData: string = rawData;
+                for (i = 0; i < formattedData.length; i++) {
+                    if (getTextWidth(formattedData.slice(0, i)) >= MAXNAMELEN) {
+                        formattedData = formattedData.slice(0, i) + "...";
+                        break;
+                    }
+                }
+
+                break;
+            case "description":
+                var formattedData: string = rawData;
+
+                const reg: RegExp = /\r?\n|\r/g;
+                formattedData = formattedData.replace(reg, " "); // Replace newlines with whitespace
+
+                for (i = 0; i < formattedData.length; i++) {
+                    if (getTextWidth(formattedData.slice(0, i)) >= MAXDESCLEN) {
+                        formattedData = formattedData.slice(0, i) + "...";
+                        break;
+                    }
+                }
+
+                break;
+            case "price":
+                let priceformattedData: number = rawData;
+                var formattedData = numberDelimiter((priceformattedData / 100));
+                
+                break;
+            case "source":
+                var formattedData: string = rawData;
+                break;
+            case "is_available":
+                var formattedData: string = rawData ? "Yes" : "No";
+                break;
+            case "":
+                var formattedData: string = "";
+                break;
+            default:
+                console.log("Error: Did not hit a valid column name")
+                var formattedData: string = "ERROR";
+                break;
+        }
+        return formattedData;
+    }
+
+
+    function displayColumn(colName: string): JSX.Element {
+        let columnStyle = widths[colName] + " border-teal-900 overflow-hidden";
+        colName = colName === "options" ? "" : colName;
+
+        return <div className={columnStyle}>
+            <div id="columnHeader" className="px-4 py-3 h-12 text-left font-bold bg-slate-400" key={colName}>
+                { colName === "is_available" ? "Available?" : 
+                (colName[0] || "").toUpperCase() + colName.substring(1) }
+
+            </div>
+
+            {products.map((product) => {
+                
+
+                return (
+                    <div className={((products.indexOf(product) % 2 === 1) ? " bg-zinc-200 " : " bg-zinc-300 ")  + " px-4 py-2 h-10 text-left font-medium"}>
+                        {formatData(product[colName], colName)}
+                    </div>
+                );
+            })}
+        </div>;
+    }
     
     
-    //onLoad={fetchProducts}
+    //onLoad={fetchProducts} 
     return (
         <div className="h-screen w-screen bg-gray-100" onLoad={fetchProducts} >
             <Sidebar pageID="1" />
             <div className="md:ml-60">
                 <h1 className="pt-20 text-4xl text-center">This is the Products page</h1>
-                <div className="m-8 bg-white border border-black">
+                <div className="m-8 py-8 bg-white border rounded-xl border-black">
                     <div className="cursor-pointer m-8 w-32 rounded text-center bg-sky-300 shadow-md hover:bg-sky-400 ring-0 outline-none appearance-none border-gray-300 border transition-all ">
                         <a href="products/create-product" className=""><div className=" ">Create new</div></a>
                     </div>
-                    {/* <div></div> for search add product and stuff */}
 
-                    <table className="w-full text-left">
-                        <thead>
-                            <tr className="mr-4 border-black">
-                                {displayedColumns.map((item: string) => {
-                                    return <th className="border" key={item}>{item}</th>;
-                                })}
-                            </tr>
-                            
-                        </thead>
-                        <tbody>
-                            {products.map((product: product) => {
-                                return displayTableData(product)
-                            })}
-                            
-                        </tbody>
-                    </table>
+                    <div id="productTable" className="w-full text-left flex">
+
+                        {displayedColumns.map((colName: string) => {
+                            return displayColumn(colName);
+                        })}
+
+                    </div>
                 </div>
-
-                <a href="products/create-product"><div className="cursor-pointer m-8 bg-slate-400">Create new</div></a>
             </div>
 
         </div>
